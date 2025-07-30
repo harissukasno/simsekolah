b589a8fdf072b34833d900fd04dad3122e291229
yaris <harissukasno@gmail.com>
2025-07-30 21:04:10 +0700

    adding cursor-pointer style

--- frontend/src/components/dashboard/mode-toggle.tsx
+++ frontend/src/components/dashboard/mode-toggle.tsx
@@ -14,6 +14,7 @@ export function ModeToggle() {
       variant="outline"
       size="icon"
       onClick={() => setTheme((prev: string) => (prev === "dark" ? "light" : "dark"))}
+      className="cursor-pointer"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
