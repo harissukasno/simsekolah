f0148d457a9077498fb4f408447510a22738c4eb
yaris <harissukasno@gmail.com>
2025-09-03 12:03:58 +0700

    editLocalhost #2

--- frontend/src/components/dashboard/nav-user.tsx
+++ frontend/src/components/dashboard/nav-user.tsx
@@ -38,7 +38,7 @@ export function NavUser({
   const handleLogout = async() => {    
     try {   
       const token = localStorage.getItem('access_token');           
-      const response = await fetch(`${process.env.SERVER_API_URL}/auth/logout`, {
+      const response = await fetch(`${process.env.SERVER_API_KEY}/auth/logout`, {
           method: 'POST',            
           headers: {              
               'Authorization': `Bearer ${token}`,
